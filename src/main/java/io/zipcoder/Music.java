ed52d12d4807e036313958903b5954f373677f1c
Chung Arguello <chung@zipcoders-MacBook-Pro-90.local>
2019-10-25 09:20:17 -0400

    finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,30 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+            int count = 0;
+            int count2 = 0;
+            int result;
+
+        for(int i = startIndex; i < playList.length; i ++){
+            if(i != 0){
+                if(playList[i].equals(selection))
+                 break;
+                else
+                 count++;
+            }else if(i == 0){
+                i = playList.length-1;
+                if(playList[i].equals(selection)){
+                    count2++;
+                }
+            }
+        }
+
+
+        if(count > count2)
+            return count;
+
+
+        return count2;
     }
 }
